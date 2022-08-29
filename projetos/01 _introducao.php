a6175653d57275265d99d2a962e2d9a804a19249
adrielacademinco <adrielsales@hotmail.com>
2022-08-28 22:40:34 -0300

    adicionando o diretório projetos

---
+++ projetos/01 _introducao.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Introdução:
+ * - Primeiros comandos.
+ *
+ */
+
+ echo "|**********************************************|" . PHP_EOL;
+ echo "|                      RECIBO                  |" . PHP_EOL;
+ echo "|**********************************************|" . PHP_EOL;
+ echo "";
+ echo "";
+ echo "";
+ echo "";
+ echo "";
+ echo "";
+ echo "";
+ echo "";
+ echo "";
+ echo "";
+ echo "";
+ echo "";
+ echo "";
+ echo "";
